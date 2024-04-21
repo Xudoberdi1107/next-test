dcdb124bc24f65c7311803369b2cfa13a296b4d9
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-04-22 00:52:14 +0500

    dev:

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,42 +1,37 @@
 import Image from "next/image";
-import img from "../../../../public/AdobeStock_103569978.jpeg";
+import img1 from "../../../../public/Adobe1.jpeg";
+import img2 from "../../../../public/Adobe1.jpeg";
+import img3 from "../../../../public/Adobe1.jpeg";
+import img4 from "../../../../public/Adobe1.jpeg";
+import img5 from "../../../../public/Adobe1.jpeg";
+import img6 from "../../../../public/Adobe1.jpeg";
+import img7 from "../../../../public/Adobe1.jpeg";
+import img8 from "../../../../public/Adobe1.jpeg";
+import img9 from "../../../../public/Adobe1.jpeg";
+import img10 from "../../../../public/Adobe1.jpeg";
+import img11 from "../../../../public/Adobe1.jpeg";
+import img12 from "../../../../public/Adobe1.jpeg";
+import img13 from "../../../../public/Adobe1.jpeg";
+import img14 from "../../../../public/Adobe1.jpeg";
 import cn from "./style.module.css";
 function About() {
   return (
     <div>
       <h1>About</h1>
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
-      <Image src={img} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img1} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img2} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img3} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img4} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img5} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img6} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img7} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img8} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img9} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img10} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img11} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img12} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img13} alt="" className={cn["img"]} loading="lazy" />
+      <Image src={img14} alt="" className={cn["img"]} loading="lazy" />
     </div>
   );
 }
