a5016fc9d56b0b09c1e65c8c4f828ea1fc07ae88
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-04-22 02:09:29 +0500

    dev:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "https://next-test-five-rose.vercel.app",
+        port: "",
+        pathname: "/public/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
